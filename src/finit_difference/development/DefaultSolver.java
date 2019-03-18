789c4184ce7ab1a73e633ba24bf7aca193aaaa04
DJEAFEA SONWA MEDRIC BRUEL <medric49@gmail.com>
2019-03-18 14:04:52 +0100

    zz

--- src/finit_difference/development/DefaultSolver.java
+++ src/finit_difference/development/DefaultSolver.java
@@ -1,6 +1,6 @@
 package finit_difference.development;
 
-public class DefaultSover implements Solver{
+public class DefaultSolver implements Solver{
     @Override
     public Vectorizable solve(long n, Function f) {
         return null;
