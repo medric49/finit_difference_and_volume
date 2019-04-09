cd0faa0d0b44ff841232bf453aca6f63261f33f8
DJEAFEA SONWA MEDRIC BRUEL <medric49@gmail.com>
2019-04-09 14:16:04 +0100

    Supprime les classes inutiles

--- src/finit_difference/development/MatrixOutOfSizeException.java
+++
@@ -1,9 +0,0 @@
-package finit_difference.development;
-
-public class MatrixOutOfSizeException extends Exception{
-
-    public MatrixOutOfSizeException(long i, long j, long n){
-        super();
-        System.out.println(i+" "+j+" "+n);
-    }
-}
--- src/finit_volume/development/Matrix.java
+++
@@ -1,9 +0,0 @@
-package finit_volume.development;
-
-import finit_difference.development.MatrixOutOfSizeException;
-
-interface Matrix{
-    double get(long i, long j) throws MatrixOutOfSizeException;
-    void set(long i, long j, double x);
-
-}
